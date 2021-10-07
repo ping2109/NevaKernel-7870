0e28fa7fa6d4727e48ca4eb55b6f8f9859bfe1bf
ping2109 <doraemonvv123@gmail.com>
2021-10-07 20:27:41 +0700

    initial commit

---
+++ neva.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ARCH=arm64
+# export CROSS_COMPILE=/home/ping2109/Downloads/old compiler/bin/aarch64_be-none-linux-gnu-
+export CROSS_COMPILE=/home/ping2109/Downloads/compiler/bin/aarch64-none-linux-gnu-
+export ANDROID_MAJOR_VERSION=q
+export ANDROID_PLATFORM_VERSION=10
+
+make exynos7870-j7xelte_defconfig
+make
