0807c8bbc2be985b743b9ef419b40ebec3baade9
ping2109 <doraemonvv123@gmail.com>
2021-10-10 14:30:03 +0700

    script: update

--- neva.sh
+++ neva.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CONFIG_LOCALVERSION=" - NevaKernel 3.0"
+export LOCALVERSION=" - NevaKernel 3.0"
 export ARCH=arm64
 # export CROSS_COMPILE=/home/ping2109/Downloads/old compiler/bin/aarch64_be-none-linux-gnu-
 export CROSS_COMPILE=/home/ping2109/Downloads/compiler/bin/aarch64-none-linux-gnu-
@@ -10,5 +10,5 @@ export ANDROID_PLATFORM_VERSION=10
 make exynos7870-j7xelte_defconfig
 make
 
-cp arch/arm64/out/Image neva/Image
+cp arch/arm64/boot/Image neva/Image
 
